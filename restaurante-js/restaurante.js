c29b8605d6ea792f650b8a465604c28d470663b0
Flavio Estrada <flavioestrada1412@gmail.com>
2026-07-03 08:30:40 -0500

    Agregando los platos con 'const' y 'let' en restaurante.js

---
+++ restaurante-js/restaurante.js
@@ -0,0 +1,23 @@
+"use stric"; 
+
+// Plato 1 - Lomo Saltado
+const nombrePlato1 = "Lomo Saltado"; 
+const precioPlato1 = 18.0;
+let stockPlato1 = 5; 
+
+// Plato 2 - Arroz con Leche 
+const nombrePlato2 = "Arroz con Leche"; 
+const precioPlato2 = 8.0;
+let stockPlato2 = 10;
+
+// Plato 3 - Sopa Criolla 
+const nombrePlato3 = "Sopa Criolla"; 
+const precioPlato3 = 12.0; 
+const stockPlato3 = 7;
+
+// Salidas 
+// Usar 'console.log' para mostrar la info de cada plato
+console.log(`Plato 1: ${nombrePlato1} | Precio: ${precioPlato1} | Stock: ${stockPlato1}`);
+console.log(`Plato 2: ${nombrePlato2} | Precio: ${precioPlato2} | Stock: ${stockPlato2}`);
+console.log(`Plato 3: ${nombrePlato3} | Precio: ${precioPlato3} | Stock: ${stockPlato3}`);
+
