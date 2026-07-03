62d5241054b1f253d1bac7dd70ae0fa62304a999
SamirOC07 <samirso0307@gmail.com>
2026-07-03 08:41:38 -0500

    Dia1-B: funciones del plato

--- restaurante-js/restaurante.js
+++ restaurante-js/restaurante.js
@@ -13,7 +13,7 @@ let stockPlato2 = 10;
 // Plato 3 - Sopa Criolla 
 const nombrePlato3 = "Sopa Criolla"; 
 const precioPlato3 = 12.0; 
-const stockPlato3 = 7;
+let stockPlato3 = 7;
 
 // Salidas 
 // Usar 'console.log' para mostrar la info de cada plato
@@ -21,3 +21,24 @@ console.log(`Plato 1: ${nombrePlato1} | Precio: ${precioPlato1} | Stock: ${stock
 console.log(`Plato 2: ${nombrePlato2} | Precio: ${precioPlato2} | Stock: ${stockPlato2}`);
 console.log(`Plato 3: ${nombrePlato3} | Precio: ${precioPlato3} | Stock: ${stockPlato3}`);
 
+// Bloque B - Funciones
+
+function describir(nombre, precio) {
+    return `${nombre} - S/ ${precio}`;
+}
+
+function estaDisponible(stock) {
+    return stock > 0;
+}
+
+function vender(stockActual) {
+    return stockActual - 1;
+}
+
+const descripcion = describir(nombrePlato1, precioPlato1);
+const disponible = estaDisponible(stockPlato1);
+const nuevoStock = vender(stockPlato1);
+
+console.log(descripcion);
+console.log(disponible);
+console.log(nuevoStock);
