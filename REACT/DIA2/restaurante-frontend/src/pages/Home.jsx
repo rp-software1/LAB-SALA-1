54a5d8e180e0ecf2347e10618904d30b939877df
SamirOC07 <samirso0307@gmail.com>
2026-07-13 09:24:16 -0500

    Bloque C (Parte B)

--- REACT/DIA2/restaurante-frontend/src/pages/Home.jsx
+++ REACT/DIA2/restaurante-frontend/src/pages/Home.jsx
@@ -1,8 +1,13 @@
-function Home(){
-    return(
-        <> 
+import PlatoCard from "../components/PlatoCard";
+import { platosMock } from "../data/platos.mock";
+function Home() {
+    return (
+        <>
             <h2>Carta del Restuarante</h2>
             <p>Aquí van los platos</p>
+            {platosMock.map(plato => (
+                <PlatoCard key={plato.id} plato={plato} />
+            ))}
         </>
 
     )
