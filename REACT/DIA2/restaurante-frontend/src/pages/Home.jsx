6ee128e1027a5e9900f6721b0f11559d604a9f66
SamirOC07 <samirso0307@gmail.com>
2026-07-13 09:28:14 -0500

    Bloque C — PlatoCard en su lugar, Home conectado, hot reload verificado

--- REACT/DIA2/restaurante-frontend/src/pages/Home.jsx
+++ REACT/DIA2/restaurante-frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { platosMock } from "../data/platos.mock";
 function Home() {
     return (
         <>
-            <h2>Carta del Restuarante</h2>
+            <h2>Carta del Restaurante "El buen gusto"</h2>
             <p>Aquí van los platos</p>
             {platosMock.map(plato => (
                 <PlatoCard key={plato.id} plato={plato} />
