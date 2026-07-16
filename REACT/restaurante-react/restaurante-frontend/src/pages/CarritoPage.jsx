5c2275446147a6694af0d42e60b6cfe5ab34e681
Flavio Estrada <flavioestrada1412@gmail.com>
2026-07-16 09:33:04 -0500

    Bloque C — carrito con cantidades total y limpiar

--- REACT/restaurante-react/restaurante-frontend/src/pages/CarritoPage.jsx
+++ REACT/restaurante-react/restaurante-frontend/src/pages/CarritoPage.jsx
@@ -15,11 +15,32 @@ export default function CarritoPage(){
     },[]);
 
     function agregarPlato(plato){
-        setCarrito([...carrito, plato]);
+        const existe = carrito.find(p => p.id === plato.id); 
+
+        if(existe){
+            setCarrito(carrito.map(p => p.id === plato.id 
+                 ? {...p, cantidad: p.cantidad + 1}
+                 : p));
+        }else{
+            setCarrito([...carrito, {...plato, cantidad: 1}])
+        }
     }
 
-    function eliminarPlato(plato){
+    function eliminarPlatoCompleto(plato){
         setCarrito(carrito.filter( p => p.id !== plato.id))
+    }
+
+    function eliminarPlatoMenosUno(plato){
+        const existe = carrito.find(p => p.id === plato.id); 
+
+        if(existe?.cantidad === 1){
+            eliminarPlatoCompleto(plato);
+        }
+        else if (existe?.cantidad > 1){
+            setCarrito(carrito.map(p => p.id === plato.id 
+                ? {...p, cantidad: p.cantidad - 1}
+                : p));
+        }
     }
 
     if (loading){
@@ -29,7 +50,7 @@ export default function CarritoPage(){
     return(
         <div>
             <h2>Carrito de compras del Menú</h2>
-            {platosMock.map(plato =>(
+            {platos.map(plato =>(
                 <div key={plato.id}>
                     <span> {plato.nombre} - {plato.precio} </span>    
                     <button onClick={() => agregarPlato(plato)}> Agregar</button>
@@ -40,10 +61,15 @@ export default function CarritoPage(){
 
             {carrito.map(plato => (
                 <div key={plato.id}>
-                    <span> {plato.nombre} - {plato.precio} </span>
-                    <button onClick={() => eliminarPlato(plato)}> Eliminar</button>
+                    <span> {plato.nombre} - S/.{plato.precio} (unitario) - {plato.cantidad} (cantidad) </span>
+                    <button onClick={() => eliminarPlatoMenosUno(plato)}> -1 </button>
+                    <button onClick={() => eliminarPlatoCompleto(plato)}> Eliminar del carrito </button>
                 </div>
             ))}
+
+            <h3>Total: S/. {carrito.reduce((sum, plato ) => sum + plato.precio * plato. cantidad, 0)}</h3>
+
+            <button onClick={() => setCarrito([])} >Limpiar Carrito</button>
             
         </div>
     )
