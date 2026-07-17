a7fc48655a78f68f58abc3d99d4348a03ab02e25
SamirOC07 <samirso0307@gmail.com>
2026-07-17 12:42:02 -0500

    Quitando el import del mock en api.js

--- REACT/restaurante-react/restaurante-frontend/src/services/api.js
+++ REACT/restaurante-react/restaurante-frontend/src/services/api.js
@@ -1,15 +1,10 @@
 import axios from "axios";
-import { platosMock } from "../data/platos.mock";
-
 // Si necesitan el 'platosMock' solo modifiquen parte de la variable como: 
 // VITE_API_URL => VITE_API
 const BASE_URL = import.meta.env.VITE_API_URL
 
 export async function getPlatos(){
 
-    if(!BASE_URL){
-        return platosMock;
-    }
     // Original 
     // const response = await axios.get(`${BASE_URL}/api/platos`); 
 
