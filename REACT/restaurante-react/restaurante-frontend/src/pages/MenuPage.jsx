faebbfd80a04ba184541c14a98cebcb2d9278e10
SamirOC07 <samirso0307@gmail.com>
2026-07-17 10:00:04 -0500

    Bloque B — MenuPage con loading error data y manejo de errores

---
+++ REACT/restaurante-react/restaurante-frontend/src/pages/MenuPage.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { getPlatos } from "../services/api";
+
+export default function MenuPaga(){
+    const [platos, setPlatos]= useState([]);
+    const [loading, setLoading]= useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(()=>{
+        async function cargarMenu(){
+            try {
+                setLoading(true);
+                setError(null)
+                const data = await getPlatos();
+                setPlatos(data); 
+            } catch (error) {
+                setError("Lo sentimos demasiado :(, no pudimos cargar el menú. Porfavor, intentalo de nuevo más tarde");
+                console.error("Error al cargar el menú:", error);
+            } finally {
+                setLoading(false);
+            }
+        }
+        cargarMenu()
+    }, [])
+    if (loading) return <div className="spinner">Cargando el restaurante...</div>
+    if (error) return <div className="error-message">{error}</div>
+    return (
+        <div className="menu-container">
+            <h2>Nuestro Menú</h2>
+            {platos.length ===0? (
+                <p>No hay platos disponibles por el momento. Vuelve más tarde</p>
+            ):(
+                platos.map((plato)=>(
+                    <div key = {plato._id} className="plato-card">
+                        <strong>{plato.nombre}</strong> - S/ {plato.precio}
+                    </div>
+                ))
+            )}
+        </div>
+    )
+}
