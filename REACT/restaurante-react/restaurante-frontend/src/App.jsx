856e6e26a3ed471f7a19fa24c9c95f3f74224d5d
Flavio Estrada <flavioestrada1412@gmail.com>
2026-07-18 10:39:35 -0500

    React Dia7 Bloque D — pagina 404 NotFound Route catch-all

--- REACT/restaurante-react/restaurante-frontend/src/App.jsx
+++ REACT/restaurante-react/restaurante-frontend/src/App.jsx
@@ -1,21 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DetalleMesa from "./pages/DetalleMesa";
 import MesasPage from "./pages/MesasPage";
-import Navbar from "./components/NavBar";
+import Layout from "./components/Layout";
 import ComandasPage from "./pages/ComandasPage";
 import CarritoPage from "./pages/CarritoPage";
 import MenuPage from "./pages/MenuPage";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar titulo="Restaurante el Buen Sabor" /> 
       <Routes>
-        <Route path="/" element={<MenuPage />} />
-        <Route path="/mesas" element={<MesasPage />} />
-        <Route path="/mesas/:id" element={<DetalleMesa />} />
-        <Route path="/comandas" element={<ComandasPage />} />
-        <Route path="/carrito" element={<CarritoPage />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<MenuPage />} />
+          <Route path="/mesas" element={<MesasPage />} />
+          <Route path="/mesas/:id" element={<DetalleMesa />} />
+          <Route path="/comandas" element={<ComandasPage />} />
+          <Route path="/carrito" element={<CarritoPage />} />
+        </Route>
+
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   )
