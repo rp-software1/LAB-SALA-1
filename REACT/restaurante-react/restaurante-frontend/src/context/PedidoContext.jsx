f40da6b8d5d06d45327669c1e845e8c924def85e
SamirOC07 <samirso0307@gmail.com>
2026-07-20 10:42:57 -0500

    Dia8-C: PedidoContext con acciones agregarPlato quitarPlato cambiarTipo

--- REACT/restaurante-react/restaurante-frontend/src/context/PedidoContext.jsx
+++ REACT/restaurante-react/restaurante-frontend/src/context/PedidoContext.jsx
@@ -14,8 +14,42 @@ const EstadoInicial = {
 export const PedidoProvider = ({children}) => { 
     const [pedido, setPedido] = useState(EstadoInicial);
 
+    const calcularTotal = (items) => {
+        return items.reduce((acc, item) => acc + (item.precioUnitario * item.cantidad), 0);
+    }
+
+    const agregarPlato = (plato) =>{
+        setPedido(prev=>{
+            const existe = prev.items.find(i=> i.platoId === plato._id);
+            const nuevosItems = existe ? prev.items.map(i => i.platoId === plato._id ? {...i, cantidad: i.cantidad +1}:i)
+            : [...prev.items, {platoId:plato_id, nombre: plato.nombre, cantidad:1, precioUnitario: plato.precio}]; 
+            return {...prev, items: nuevosItems, total: calcularTotal(nuevosItems)};
+        });
+    };
+
+    const quitarPlato =(platoId) => {
+        setPedido(prev => {
+            const nuevosItems = prev.items
+            .map(i => i.platoId === platoId ? {...i, cantidad: i.cantidad - 1}:i)
+            .filter(i=> i.cantidad > 0)
+            return {...prev, items: nuevosItems, total: calcularTotal(nuevosItems)}
+        })
+    }
+    
+    const cambiarTipo = (tipo) => {
+        setPedido(prev => ({
+            ...prev,
+            tipo,
+            mesaId: tipo === 'para_llevar' ? null : prev.mesaId
+        }))
+    }
+
+    const limpiarPedido = () => {
+        setPedido(EstadoInicial);
+    };
+
     return(
-        <PedidoContext.Provider value={{pedido, setPedido}}>
+        <PedidoContext.Provider value={{pedido, setPedido, agregarPlato, quitarPlato, cambiarTipo, limpiarPedido}}>
             {children}
         </PedidoContext.Provider>
     );
